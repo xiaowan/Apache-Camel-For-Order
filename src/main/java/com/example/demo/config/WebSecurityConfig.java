5213a584eed03403f651ec2019120063d49ced53
李聪 <congli@91jkys.com>
2021-03-07 23:57:34 +0800

    add spring security

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author licong
+ * @date 2021/3/7 下午11:53
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/preOrder", "/submitOrder").permitAll()
+                .anyRequest().authenticated();
+    }
+}
